6b0cea24a98f813685ffc1b4962c173d054ee814
lily (๑◡๑) <ilovesuno@proton.me>
2025-08-03 10:34:55 -0700

    Upload the second version of my wegsbite :3
    Maybe fix the resizing issue with the browser window, maybe not, who knows?

---
+++ base.js
@@ -0,0 +1,16 @@
+const learn_more = document.getElementById("learn-more");
+learn_more.style.top = "10%";
+learn_more.style.left = "25%";
+// learn_more.style.transform = "translate(-50%, -50%)";
+
+const socials = document.getElementById("socials");
+socials.style.top = "41%";
+socials.style.left = "50%";
+
+const projects = document.getElementById("projects");
+projects.style.left = "14%";
+projects.style.top = "41%";
+
+const open_project_repo = document.getElementById("open-project-repo");
+open_project_repo.style.top = "60.5%"
+open_project_repo.style.left = "19.5%";
